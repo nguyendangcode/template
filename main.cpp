ecc32a9ae9eff561b73e050330031d0bfd0c9fb3
nguyendangcode <dinh2671@gmail.com>
2025-08-17 07:27:02 +0000

    mang

--- main.cpp
+++ main.cpp
@@ -3,30 +3,53 @@ using namespace std;
 
 template <class T>
 class Mang {
-    int sl;     
-    T* data;    
+    int sl;        
+    int capacity; 
+    T* data;
+    void resize(int newCap) {
+        if (newCap <= capacity) return;
+        T* newData = new T[newCap];
+        for (int i = 0; i < sl; i++) newData[i] = data[i];
+        delete[] data;
+        data = newData;
+        capacity = newCap;
+    }
 public:
-
     Mang(int n = 0) {
-        sl = n;
-        data = new T[sl];
+        sl = 0;
+        capacity = (n > 0) ? n : 1;
+        data = new T[capacity];
     }
     ~Mang() {
         delete[] data;
     }
     T& operator[](int i) {
+        if (i >= capacity) {
+            resize(i + 1);
+        }
+        if (i >= sl) {
+            sl = i + 1;
+        }
         return data[i];
     }
+
     int size() const { return sl; }
+    int getCapacity() const { return capacity; }
+
+    void print() const {
+        for (int i = 0; i < sl; i++) cout << data[i] << " ";
+        cout << endl;
+    }
 };
 
 int main() {
-    Mang<float> a(4);   
-    for (int i = 0; i < a.size(); i++) {
-        a[i] = i * 10.11;   
-    }
-    for (int i = 0; i < a.size(); i++) {
-        cout << "a[" << i << "] = " << a[i] << endl;
-    }
+    Mang<int> a;
+    a[0] = 10;   
+    a[1] = 20;   
+    a[9] = 50;   
+    cout << "Mang hien tai: ";
+    a.print();
+    cout << "So phan tu = " << a.size() 
+         << ", capacity = " << a.getCapacity() << endl;
     return 0;
 }
